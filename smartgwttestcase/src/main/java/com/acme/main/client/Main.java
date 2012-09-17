dc054a94cb52f53393819318664c9348e50fe31d
Davide Cavestro <davide.cavestro@gmail.com>
2012-09-17 10:44:16 +0200

    Initial import

---
+++ smartgwttestcase/src/main/java/com/acme/main/client/Main.java
@@ -0,0 +1,18 @@
+package com.acme.main.client;
+
+import java.util.logging.Logger;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Main implements EntryPoint {
+
+	private final Logger logger = Logger.getLogger (getClass ().getName ());
+
+	public void onModuleLoad () {
+		
+		logger.warning ("REMOVE ME");
+		//TODO put test code here
+		
+	}
+
+}
