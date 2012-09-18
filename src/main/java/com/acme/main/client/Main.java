dcdc40946d9cc397f98dd3e46e338fc3f7f54652
Davide Cavestro <davide.cavestro@gmail.com>
2012-09-18 09:39:26 +0200

    * Added smartgwt dependencies * Improved documentation * Added example code

--- src/main/java/com/acme/main/client/Main.java
+++ src/main/java/com/acme/main/client/Main.java
@@ -8,11 +8,51 @@ public class Main implements EntryPoint {
 
 	private final Logger logger = Logger.getLogger (getClass ().getName ());
 
+	@Override
 	public void onModuleLoad () {
-		
+
+		//replace the following code with the test logic
 		logger.warning ("REMOVE ME");
-		//TODO put test code here
-		
+
+//		//datasource initialization
+//		final DataSource dataSource = new DataSource ();
+//		{//datasource fields initialization
+//			final List<ListGridRecord> testData = new ArrayList<ListGridRecord> ();
+//			{
+//				final ListGridRecord record = new ListGridRecord ();
+//				record.setAttribute ("id", 1);
+//				record.setAttribute ("name", "1st record");
+//				testData.add (record);
+//			}
+//			{
+//				final ListGridRecord record = new ListGridRecord ();
+//				record.setAttribute ("id", 2);
+//				record.setAttribute ("name", "2nd record");
+//				testData.add (record);
+//			}
+//			dataSource.setTestData (testData.toArray (new ListGridRecord[0]));
+//		}
+//		dataSource.setClientOnly (true);
+//		{
+//			final DataSourceIntegerField dsfId = new DataSourceIntegerField ("id");
+//			dsfId.setPrimaryKey (true);
+//			dataSource.addField (dsfId);
+//			final DataSourceTextField dsfName = new DataSourceTextField ("name");
+//
+//			dataSource.addField (dsfName);
+//		}
+//		
+//		//UI initialization
+//		final ListGrid grid = new ListGrid ();
+//		grid.setAutoFetchData (true);
+//		grid.setDataSource (dataSource);
+//		
+//		final VLayout layout = new VLayout ();
+//		layout.addMember (grid);
+//		layout.setWidth100 ();
+//		layout.setHeight100 ();
+//		layout.show ();
+
 	}
 
 }
